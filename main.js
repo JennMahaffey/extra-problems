c9cffdd77ef7ebf6b1000254b3c0b6862cb867a2
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-10-16 11:07:34 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,64 @@
+function isEvenlyDivisible(num1, num2) {
+if(num1 % num2 === 0){
+  return true;
+}else{
+  return false;
+}
+}
+
+function halfSquare() {
+}
+
+function isLong() {
+}
+
+function exclaim() {
+}
+
+function countWords() {
+}
+
+function containsDigit(str) {
+}
+
+function containsLowerCase() {
+}
+
+function containsUpperCase() {
+}
+
+function containsNonAlphanumeric() {
+}
+
+function containsSpace() {
+}
+
+function digits(num) {
+}
+
+function truncate() {
+}
+
+function isValidPassword() {
+}
+
+function onlyPunchy() {
+}
+
+
+module.exports = {
+  isEvenlyDivisible,
+  halfSquare,
+  exclaim,
+  isLong,
+  containsDigit,
+  containsLowerCase,
+  containsUpperCase,
+  containsNonAlphanumeric,
+  containsSpace,
+  countWords,
+  digits,
+  truncate,
+  isValidPassword,
+  onlyPunchy,
+}
